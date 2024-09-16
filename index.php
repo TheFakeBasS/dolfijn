c4dd0ad5411e213d68681f789ac62420eb345844
Mike_Vergeer <mikevergeer0204@gmail.com>
2024-09-16 14:09:22 +0200

    wire frame uitgewerkt

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesheet.css">
+    <title>Dolphijn website</title>
+</head>
+    <?php include 'navbar.php'; ?>
+    <header class="hero">
+        <img src="dolphijnheader.png" alt="Hero" id="dolphijnheader">
+    </header>
 
+    <div class="textcontainer">
+    <div id="introtext">
+        <h1>Intro Dolphijn</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium doloremque laboriosam consequuntur sed minus suscipit vero! Ex, vitae laborum repudiandae laudantium quis, voluptatum assumenda minima incidunt quos, veritatis qui adipisci? Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ex id ipsa quibusdam neque cum, quidem nam tempora quo impedit magni illum, reprehenderit maiores? Ullam, temporibus unde. Ea, fugit Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed delectus in obcaecati esse fugiat laborum aliquid, aspernatur, sequi autem necessitatibus dolorem dolores blanditiis ipsum vero et dolore, voluptate repellendus! Natus. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur ut perspiciatis saepe iste quia cupiditate vel, veritatis dolore suscipit nam, nesciunt, voluptates est. Minus vitae sint sit dolores corrupti at.
+    </div>
+</div>
+</body>
+</html>
