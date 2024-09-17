ae365344ba57fe0238a491a3413215c3a1c78db1
Mike_Vergeer <mikevergeer0204@gmail.com>
2024-09-17 13:19:00 +0200

    plaatjes toegvoegd en gestyled voor links

--- index.php
+++ index.php
@@ -17,5 +17,28 @@
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium doloremque laboriosam consequuntur sed minus suscipit vero! Ex, vitae laborum repudiandae laudantium quis, voluptatum assumenda minima incidunt quos, veritatis qui adipisci? Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ex id ipsa quibusdam neque cum, quidem nam tempora quo impedit magni illum, reprehenderit maiores? Ullam, temporibus unde. Ea, fugit Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed delectus in obcaecati esse fugiat laborum aliquid, aspernatur, sequi autem necessitatibus dolorem dolores blanditiis ipsum vero et dolore, voluptate repellendus! Natus. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur ut perspiciatis saepe iste quia cupiditate vel, veritatis dolore suscipit nam, nesciunt, voluptates est. Minus vitae sint sit dolores corrupti at.
     </div>
 </div>
+
+<div class="Afbeeldingcontainers">
+    <div class="linkplaatjes">
+        <div class="image-wrapper">
+            <img src="DolphinAlgemeen.png" alt="Algemeen">
+            <div class="text-overlay">Algemeen</div>
+        </div>
+        <div class="image-wrapper">
+            <img src="dolphijnheader.png" alt="Geschiedenis">
+            <div class="text-overlay">Geschiedenis</div>
+        </div>
+        <div class="image-wrapper">
+            <img src="dolphin.static.png" alt="Leefgebied">
+            <div class="text-overlay">Leefgebied</div>
+        </div>
+        <div class="image-wrapper">
+            <img src="22b488357d0e769f35e18d0cf22e2018-1877-dubai-dubai-dolphinarium-tickets--swimming-with-dolphins-10.avif" alt="">
+            <div class="text-overlay">Dolphin Interaction</div>
+        </div>
+    </div>
+</div>
+
+</div>
 </body>
 </html>
