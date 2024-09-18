36e56f8edd753e172edce44ec00fdea922d9aba3
Thomas_van_Sprang <EMAIL_ADDRESS>
2024-09-18 09:57:48 +0200

    Gechiedenis-pagina eerste deel

---
+++ Geschiedenis.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Geschiedenis</title>
+    <link rel="stylesheet" href="Geschiedenis.css">
+</head>
+
+<body>
+    <?php include 'navbar.php'; ?>
+    <header class="hero">
+        <h1 class="page-title">De Geschiedenis van de dolfijn</h1>
+        <img src="dolphin-historypage(1).jpg" alt="" id="dolphijnheader">
+    </header>
+    <div class="textcontainer">
+        <div id="introtext">
+            <h1>Intro</h1>
+            Dolfijnen zijn een van de meest fascinerende en intelligente dieren in de oceaan. Ze behoren tot de familie van de walvisachtigen en hebben een evolutie doorgemaakt die miljoenen jaren beslaat. Deze mariene zoogdieren staan bekend om hun sociale gedrag, opmerkelijke communicatieve vaardigheden en het vermogen om met behulp van echolocatie te navigeren en te jagen. In dit overzicht verkennen we de lange geschiedenis van de dolfijnen, van hun vroege evolutie op het land tot hun prominente rol in zowel de natuur als menselijke cultuur.
+        </div>
+    </div>
+    <div class="combirow">
+        <div class="text-left">
+            <div class="content">
+                <h1>Evolutie van de Dolfijn</h1>
+                Dolfijnen zijn onderdeel van de walvisachtigen (Cetacea), die zo'n 50 miljoen jaar geleden evolueerden uit viervoetige landzoogdieren. De voorouders van dolfijnen leefden aanvankelijk op het land, zoals het geslacht Pakicetus, dat vooral werd aangetroffen in wat nu Pakistan is. Na verloop van tijd ontwikkelden deze vroege zoogdieren zich tot amfibische wezens zoals Ambulocetus, die zowel op het land als in het water leefden. Uiteindelijk ontstonden volledig aquatische voorouders, zoals Dorudon, die sterk lijken op moderne dolfijnen en rond 40 miljoen jaar geleden volledig aangepast waren aan het leven in de oceaan.
+            </div>
+        </div>
+        <div class="image-right">
+        <img src="dolphin-historypage( 2).jpg" alt="" id="right-image">
+        </div>
+    </div>
+</body>
+
+</html>
