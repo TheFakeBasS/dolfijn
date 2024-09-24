0e54b1d09922eaaf7dbbe9f4a1af8511a4653447
Thomas_van_Sprang <EMAIL_ADDRESS>
2024-09-24 09:38:00 +0200

    Footer af

--- footer.php
+++ footer.php
@@ -6,8 +6,20 @@
         <a href="Leefgebieden.php">Leefgebieden</a>
         <a href="Activiteiten.php">Activiteiten</a>
     </ul>
-    <p class="footer-title">Contact</p>
-    <div class="contact"></div>
+    <div class="contact">
+        <p class="footer-title">Contact</p>
+        <div class="item">WhatsApp: 06 54 98 74 89</div>
+        <div class="item">Telefoon: 088 2333 444</div>
+        <div class="item">Mail: studieinfo@rocmn.nl</div>
+    </div>
+    <div class="madeby">
+        <p class="footer-title">Gemaakt door:</p>
+        <div class="item">Mike Vergeer</div>
+        <div class="item">Bas</div>
+        <div class="item">Isaiah</div>
+        <div class="item">Hamza</div>
+        <div class="item">Thomas van Sprang</div>
+    </div>
 </footer>
 
 <style>
@@ -25,14 +37,19 @@
         margin: 0;
     }
 
-    .contact {
+    .contact,
+    .madeby {
+        display: flex;
+        flex-direction: column;
         color: #fff;
+        gap: 7px;
     }
 
     ul {
         display: flex;
         flex-direction: column;
         gap: 7px;
+        margin: 0;
     }
 
     footer a {
